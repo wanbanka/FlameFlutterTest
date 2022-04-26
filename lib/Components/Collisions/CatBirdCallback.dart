f7a3969561a06b0ff6857032b1665c7d284381aa
wanbanka <hurt.stress@gmail.com>
2022-04-26 13:53:00 +0200

    FINISH forge

--- lib/Components/Collisions/CatBirdCallback.dart
+++ lib/Components/Collisions/CatBirdCallback.dart
@@ -7,13 +7,50 @@ import '../Sprites/BirdBody.dart';
 //corps et comment gérer ces collisions (début, fin...)
 
 class CatBirdCallback extends ContactCallback<CatBody, BirdBody> {
+  double _totalImpactImpulse = 0;
+
+  int _contactCounter = 1;
+
   @override
   void begin(CatBody a, BirdBody b, Contact contact) {
+    _totalImpactImpulse = 0;
+
+    _contactCounter = 1;
+
+    contact.manifold.points.forEach((point) {
+      print("Local point: ${point.localPoint}");
+    });
+
+    var worldContact = WorldManifold();
+
+    contact.getWorldManifold(worldContact);
+
+    print("World points: ${worldContact.points}");
+
+    final cat = a.body.linearVelocityFromWorldPoint(worldContact.points[0]);
+
+    final bird = b.body.linearVelocityFromWorldPoint(worldContact.points[0]);
+
+    print("Impact velocity: ${cat - bird}");
+
     a.speak();
+  }
+
+  @override
+  void postSolve(
+      CatBody a, BirdBody b, Contact contact, ContactImpulse impulse) {
+    // TODO: implement postSolve
+
+    _totalImpactImpulse += impulse.normalImpulses[0].abs();
+
+    _contactCounter++;
   }
 
   @override
   void end(CatBody a, BirdBody b, Contact contact) {
     // TODO: implement end
+
+    print(
+        "End of contact: impulse $_totalImpactImpulse and counter $_contactCounter");
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mvc_application/view.dart' show AppState, AppMVC;
+
+import 'package:flame_splash_screen/flame_splash_screen.dart';
+
 import 'Views/HomePage.dart';
 
 void main() {
@@ -18,20 +21,39 @@ class MyApp extends AppMVC {
 class View extends AppState {
   View()
       : super(
-          title: 'Flutter Demo',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            // This is the theme of your application.
-            //
-            // Try running your application with "flutter run". You'll see the
-            // application has a blue toolbar. Then, without quitting the app, try
-            // changing the primarySwatch below to Colors.green and then invoke
-            // "hot reload" (press "r" in the console where you ran "flutter run",
-            // or simply save your changes to "hot reload" in a Flutter IDE).
-            // Notice that the counter didn't reset back to zero; the application
-            // is not restarted.
-            primarySwatch: Colors.blue,
-          ),
-          home: const HomePage(title: 'Flutter Demo Home Page'),
-        );
+            title: 'Flutter Demo',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              // This is the theme of your application.
+              //
+              // Try running your application with "flutter run". You'll see the
+              // application has a blue toolbar. Then, without quitting the app, try
+              // changing the primarySwatch below to Colors.green and then invoke
+              // "hot reload" (press "r" in the console where you ran "flutter run",
+              // or simply save your changes to "hot reload" in a Flutter IDE).
+              // Notice that the counter didn't reset back to zero; the application
+              // is not restarted.
+              primarySwatch: Colors.blue,
+            ),
+            home: FlameSplashScreen(
+              onFinish: (context) =>
+                  Navigator.pushReplacementNamed(context, "/game"),
+              theme: FlameSplashTheme.dark,
+              showBefore: (context) {
+                return Text("WOW");
+              },
+            ),
+            onGenerateRoute: (RouteSettings settings) {
+              var routeName = settings.name!;
+
+              switch (routeName) {
+                case "/game":
+                  return MaterialPageRoute(
+                      builder: (_) =>
+                          HomePage(title: 'Flutter Demo Home Page'));
+
+                  break;
+                default:
+              }
+            });
 }
